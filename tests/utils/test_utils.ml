84b7688e8604a8127f3c593d45742e523d8cd4c7
Thierry Martinez <Thierry.Martinez@inria.fr>
2020-02-19 17:43:27 +0300

    Fix test

--- tests/utils/test_utils.ml
+++ tests/utils/test_utils.ml
@@ -3,10 +3,10 @@
 let () =
   Test_framework.assert_eq Stdcompat.Int.equal Format.pp_print_int
     [%meta Metapp_utils.filter.expr Metapp_utils.filter
-       [%e (1, 2 [@when false])]] 1
+       [%e (1, 2 [@if false])]] 1
 
 [%%meta Metapp_utils.filter.structure_item Metapp_utils.filter
-  [%stri let a = 1 and[@when false] b = c]]
+  [%stri let a = 1 and[@if false] b = c]]
 
 let () =
   Test_framework.assert_eq Stdcompat.Int.equal Format.pp_print_int a 1
