fc47bc26896217638784bc15668ef62e0919382a
Edwin Török <edwin.torok@cloud.com>
2024-01-02 21:37:44 +0000

    Add a testcase for #9
    Check that type and record filtering works
    
    Signed-off-by: Edwin Török <edwin.torok@cloud.com>

--- tests/utils/test_utils.ml
+++ tests/utils/test_utils.ml
@@ -11,6 +11,41 @@ let () =
 [%%meta (new Metapp.filter)#structure_item
   [%stri let a = 1 and[@if false] b = c]]
 
+type other = AX | CX
+
+[%%meta (new Metapp.filter)#structure_item
+  [%stri type test = other =  AX | BX [@if false] | CX]
+]
+
+type other2 = { a: int; b: float }
+
+[%%meta (new Metapp.filter)#structure_item
+  [%stri type test2 = other2 = { a: int; b: float; c: bool [@if false]}]
+]
+
+let make2 x =
+  [%meta (new Metapp.filter)#expression [%e
+    { a = 4; b = 5.0; c = 4 [@if false] }
+  ]]
+
+let something' _ _ = ()
+let something _ = ()
+
+[%%meta Metapp.Stri.of_list @@ (new Metapp.filter)#structure [%str
+  type t =
+    | A of int
+    | B of int * int
+        [@if [%meta Metapp.Exp.of_bool (Sys.ocaml_version >= "4.04.0")]]
+    (* ... *)
+
+  let somefunction v =
+    match (v: t) with
+    | A x -> something x
+    | B (y,z)
+      [@if [%meta Metapp.Exp.of_bool (Sys.ocaml_version >= "4.04.0")]] ->
+        something' y z
+    (* ... *) ]]
+
 let () =
   Test_framework.assert_eq Int.equal Format.pp_print_int a 1
 
